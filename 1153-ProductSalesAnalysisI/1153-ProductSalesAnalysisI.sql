be0da17b5d6bb817e44edb14baedf64a40aa3eea
keerthikroshni <keerthivisu33@gmail.com>
2026-07-17 14:58:52 +0530

    Create 1153-ProductSalesAnalysisI

---
+++ 1153-ProductSalesAnalysisI/1153-ProductSalesAnalysisI.sql
@@ -0,0 +1,6 @@
+-- Last updated: 7/17/2026, 2:57:19 PM
+# Write your MySQL query statement below
+SELECT p.product_name, s.year, s.price
+FROM Sales s
+JOIN Product p
+ON s.product_id = p.product_id;

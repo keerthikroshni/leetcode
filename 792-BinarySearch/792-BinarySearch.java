db1a4087cc14c886aa5cc7870d734d0043dba3f3
keerthikroshni <keerthivisu33@gmail.com>
2026-07-17 14:59:03 +0530

    Create 792-BinarySearch

---
+++ 792-BinarySearch/792-BinarySearch.java
@@ -0,0 +1,20 @@
+// Last updated: 7/17/2026, 2:57:30 PM
+class Solution {
+    public int search(int[] nums, int target) {
+        int left =0;
+        int right=nums.length-1;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]>target){
+                right=mid-1;
+            }
+            else{
+                left=mid+1;
+            }
+        }
+        return -1;
+    }
+}

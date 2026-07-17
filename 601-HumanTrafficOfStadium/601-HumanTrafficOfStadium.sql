b961ebf9ac1b45a7da24f90ade5f63cc804c5948
keerthikroshni <keerthivisu33@gmail.com>
2026-07-17 14:59:21 +0530

    Create 601-HumanTrafficOfStadium

---
+++ 601-HumanTrafficOfStadium/601-HumanTrafficOfStadium.sql
@@ -0,0 +1,17 @@
+-- Last updated: 7/17/2026, 2:57:48 PM
+# Write your MySQL query statement below
+WITH cte AS (
+    SELECT *,
+           id - ROW_NUMBER() OVER (ORDER BY id) AS grp
+    FROM Stadium
+    WHERE people >= 100
+)
+SELECT id, visit_date, people
+FROM cte
+WHERE grp IN (
+    SELECT grp
+    FROM cte
+    GROUP BY grp
+    HAVING COUNT(*) >= 3
+)
+ORDER BY visit_date;

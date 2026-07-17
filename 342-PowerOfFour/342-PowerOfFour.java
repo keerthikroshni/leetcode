3273b5f876788529f62d4f5cb062b3dc274a476b
keerthikroshni <keerthivisu33@gmail.com>
2026-07-17 14:59:35 +0530

    Create 342-PowerOfFour

---
+++ 342-PowerOfFour/342-PowerOfFour.java
@@ -0,0 +1,15 @@
+// Last updated: 7/17/2026, 2:58:02 PM
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if(n<1)
+        return false;
+        if(n==1)
+        return true;
+        while(n%4==0)
+        n=n/4;
+        if(n==1)
+        return true;
+        else
+        return false;
+    }
+}

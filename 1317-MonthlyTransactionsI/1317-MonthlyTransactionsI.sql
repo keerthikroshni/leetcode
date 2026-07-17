8c39847a0337dc3dc611eb82857225b8ee845ac1
keerthikroshni <keerthivisu33@gmail.com>
2026-07-17 14:58:34 +0530

    Create 1317-MonthlyTransactionsI

---
+++ 1317-MonthlyTransactionsI/1317-MonthlyTransactionsI.sql
@@ -0,0 +1,13 @@
+-- Last updated: 7/17/2026, 2:57:01 PM
+# Write your MySQL query statement below
+SELECT
+    DATE_FORMAT(trans_date, '%Y-%m') AS month,
+    country,
+    COUNT(*) AS trans_count,
+    SUM(CASE WHEN state = 'approved' THEN 1 ELSE 0 END) AS approved_count,
+    SUM(amount) AS trans_total_amount,
+    SUM(CASE WHEN state = 'approved' THEN amount ELSE 0 END) AS approved_total_amount
+FROM Transactions
+GROUP BY
+    DATE_FORMAT(trans_date, '%Y-%m'),
+    country;

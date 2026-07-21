5bdd96cad0c518489ea90ddf7ee05cf07c740981
keerthikroshni <keerthivisu33@gmail.com>
2026-07-21 09:59:16 +0530

    File updated at 7/21/2026, 9:57:41 AM

--- 28-FindtheIndexoftheFirstOccurrenceinaString/28-FindtheIndexoftheFirstOccurrenceinaString.java
+++ 28-FindtheIndexoftheFirstOccurrenceinaString/28-FindtheIndexoftheFirstOccurrenceinaString.java
@@ -1,19 +1,28 @@
-// Last updated: 7/21/2026, 9:56:24 AM
+// Last updated: 7/21/2026, 9:57:41 AM
 1class Solution {
-2    public int jump(int[] nums) {
-3        int jumps = 0;
-4        int currentEnd = 0;
-5        int farthest = 0;
-6
-7        for (int i = 0; i < nums.length - 1; i++) {
-8            farthest = Math.max(farthest, i + nums[i]);
-9
-10            if (i == currentEnd) {
-11                jumps++;
-12                currentEnd = farthest;
-13            }
-14        }
-15
-16        return jumps;
-17    }
-18}
+2    List<List<Integer>> result = new ArrayList<>();
+3
+4    public List<List<Integer>> permute(int[] nums) {
+5        backtrack(nums, new ArrayList<>(), new boolean[nums.length]);
+6        return result;
+7    }
+8
+9    private void backtrack(int[] nums, List<Integer> current, boolean[] used) {
+10        if (current.size() == nums.length) {
+11            result.add(new ArrayList<>(current));
+12            return;
+13        }
+14
+15        for (int i = 0; i < nums.length; i++) {
+16            if (used[i]) {
+17                continue;
+18            }
+19
+20            used[i] = true;
+21            current.add(nums[i]);
+22            backtrack(nums, current, used);
+23            current.remove(current.size() - 1);
+24            used[i] = false;
+25        }
+26    }
+27}

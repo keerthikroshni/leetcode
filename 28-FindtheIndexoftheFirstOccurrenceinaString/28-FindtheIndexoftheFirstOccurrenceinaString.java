9632cfeb886ebd6b564c2f934ba7b6adf045924f
keerthikroshni <keerthivisu33@gmail.com>
2026-07-21 09:57:02 +0530

    File updated at 7/21/2026, 9:55:27 AM

--- 28-FindtheIndexoftheFirstOccurrenceinaString/28-FindtheIndexoftheFirstOccurrenceinaString.java
+++ 28-FindtheIndexoftheFirstOccurrenceinaString/28-FindtheIndexoftheFirstOccurrenceinaString.java
@@ -1,32 +1,30 @@
-// Last updated: 7/21/2026, 9:52:38 AM
+// Last updated: 7/21/2026, 9:55:27 AM
 1class Solution {
-2    public String multiply(String num1, String num2) {
-3        if (num1.equals("0") || num2.equals("0")) {
-4            return "0";
-5        }
-6
-7        int m = num1.length();
-8        int n = num2.length();
-9        int[] result = new int[m + n];
-10
-11        for (int i = m - 1; i >= 0; i--) {
-12            for (int j = n - 1; j >= 0; j--) {
-13                int mul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
-14                int sum = mul + result[i + j + 1];
-15
-16                result[i + j + 1] = sum % 10;
-17                result[i + j] += sum / 10;
-18            }
-19        }
-20
-21        StringBuilder sb = new StringBuilder();
+2    public boolean isMatch(String s, String p) {
+3        int i = 0, j = 0;
+4        int star = -1, match = 0;
+5
+6        while (i < s.length()) {
+7            if (j < p.length() && (p.charAt(j) == '?' || p.charAt(j) == s.charAt(i))) {
+8                i++;
+9                j++;
+10            } else if (j < p.length() && p.charAt(j) == '*') {
+11                star = j;
+12                match = i;
+13                j++;
+14            } else if (star != -1) {
+15                j = star + 1;
+16                match++;
+17                i = match;
+18            } else {
+19                return false;
+20            }
+21        }
 22
-23        for (int num : result) {
-24            if (!(sb.length() == 0 && num == 0)) {
-25                sb.append(num);
-26            }
-27        }
-28
-29        return sb.toString();
-30    }
-31}
+23        while (j < p.length() && p.charAt(j) == '*') {
+24            j++;
+25        }
+26
+27        return j == p.length();
+28    }
+29}

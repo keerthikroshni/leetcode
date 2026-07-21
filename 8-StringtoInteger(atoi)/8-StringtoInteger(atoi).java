1ae2ca20cf81d5eb9927faf619c3a36ea8e508ff
keerthikroshni <keerthivisu33@gmail.com>
2026-07-21 09:33:54 +0530

    File updated at 7/21/2026, 9:32:18 AM

--- 8-StringtoInteger(atoi)/8-StringtoInteger(atoi).java
+++ 8-StringtoInteger(atoi)/8-StringtoInteger(atoi).java
@@ -1,34 +1,24 @@
-// Last updated: 7/21/2026, 9:31:27 AM
+// Last updated: 7/21/2026, 9:32:18 AM
 1class Solution {
-2    public int myAtoi(String s) {
-3        int i = 0;
-4        int n = s.length();
-5
-6        while (i < n && s.charAt(i) == ' ') {
-7            i++;
-8        }
-9
-10        int sign = 1;
-11        if (i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
-12            if (s.charAt(i) == '-') {
-13                sign = -1;
-14            }
-15            i++;
-16        }
-17
-18        int result = 0;
-19
-20        while (i < n && Character.isDigit(s.charAt(i))) {
-21            int digit = s.charAt(i) - '0';
-22
-23            if (result > (Integer.MAX_VALUE - digit) / 10) {
-24                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
-25            }
-26
-27            result = result * 10 + digit;
-28            i++;
-29        }
-30
-31        return result * sign;
-32    }
-33}
+2    List<String> result = new ArrayList<>();
+3
+4    public List<String> generateParenthesis(int n) {
+5        backtrack("", 0, 0, n);
+6        return result;
+7    }
+8
+9    private void backtrack(String current, int open, int close, int n) {
+10        if (current.length() == 2 * n) {
+11            result.add(current);
+12            return;
+13        }
+14
+15        if (open < n) {
+16            backtrack(current + "(", open + 1, close, n);
+17        }
+18
+19        if (close < open) {
+20            backtrack(current + ")", open, close + 1, n);
+21        }
+22    }
+23}

02e30b9ccf1caaffea0792aaca1961cd38100338
keerthikroshni <keerthivisu33@gmail.com>
2026-07-21 09:48:34 +0530

    File updated at 7/21/2026, 9:46:59 AM

--- 28-FindtheIndexoftheFirstOccurrenceinaString/28-FindtheIndexoftheFirstOccurrenceinaString.java
+++ 28-FindtheIndexoftheFirstOccurrenceinaString/28-FindtheIndexoftheFirstOccurrenceinaString.java
@@ -1,34 +1,31 @@
-// Last updated: 7/21/2026, 9:44:49 AM
+// Last updated: 7/21/2026, 9:46:59 AM
 1class Solution {
-2    public void nextPermutation(int[] nums) {
-3        int i = nums.length - 2;
-4
-5        while (i >= 0 && nums[i] >= nums[i + 1]) {
-6            i--;
-7        }
+2    public int search(int[] nums, int target) {
+3        int left = 0;
+4        int right = nums.length - 1;
+5
+6        while (left <= right) {
+7            int mid = left + (right - left) / 2;
 8
-9        if (i >= 0) {
-10            int j = nums.length - 1;
-11
-12            while (nums[j] <= nums[i]) {
-13                j--;
-14            }
-15
-16            swap(nums, i, j);
-17        }
-18
-19        reverse(nums, i + 1, nums.length - 1);
-20    }
-21
-22    private void swap(int[] nums, int i, int j) {
-23        int temp = nums[i];
-24        nums[i] = nums[j];
-25        nums[j] = temp;
-26    }
+9            if (nums[mid] == target) {
+10                return mid;
+11            }
+12
+13            if (nums[left] <= nums[mid]) {
+14                if (target >= nums[left] && target < nums[mid]) {
+15                    right = mid - 1;
+16                } else {
+17                    left = mid + 1;
+18                }
+19            } else {
+20                if (target > nums[mid] && target <= nums[right]) {
+21                    left = mid + 1;
+22                } else {
+23                    right = mid - 1;
+24                }
+25            }
+26        }
 27
-28    private void reverse(int[] nums, int left, int right) {
-29        while (left < right) {
-30            swap(nums, left++, right--);
-31        }
-32    }
-33}
+28        return -1;
+29    }
+30}

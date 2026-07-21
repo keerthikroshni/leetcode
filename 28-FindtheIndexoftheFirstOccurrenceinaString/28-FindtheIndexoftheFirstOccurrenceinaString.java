27d7b33f45d2c6649437558dcfb3317f7a6a1200
keerthikroshni <keerthivisu33@gmail.com>
2026-07-21 09:57:59 +0530

    File updated at 7/21/2026, 9:56:24 AM

--- 28-FindtheIndexoftheFirstOccurrenceinaString/28-FindtheIndexoftheFirstOccurrenceinaString.java
+++ 28-FindtheIndexoftheFirstOccurrenceinaString/28-FindtheIndexoftheFirstOccurrenceinaString.java
@@ -1,30 +1,19 @@
-// Last updated: 7/21/2026, 9:55:27 AM
+// Last updated: 7/21/2026, 9:56:24 AM
 1class Solution {
-2    public boolean isMatch(String s, String p) {
-3        int i = 0, j = 0;
-4        int star = -1, match = 0;
-5
-6        while (i < s.length()) {
-7            if (j < p.length() && (p.charAt(j) == '?' || p.charAt(j) == s.charAt(i))) {
-8                i++;
-9                j++;
-10            } else if (j < p.length() && p.charAt(j) == '*') {
-11                star = j;
-12                match = i;
-13                j++;
-14            } else if (star != -1) {
-15                j = star + 1;
-16                match++;
-17                i = match;
-18            } else {
-19                return false;
-20            }
-21        }
-22
-23        while (j < p.length() && p.charAt(j) == '*') {
-24            j++;
-25        }
-26
-27        return j == p.length();
-28    }
-29}
+2    public int jump(int[] nums) {
+3        int jumps = 0;
+4        int currentEnd = 0;
+5        int farthest = 0;
+6
+7        for (int i = 0; i < nums.length - 1; i++) {
+8            farthest = Math.max(farthest, i + nums[i]);
+9
+10            if (i == currentEnd) {
+11                jumps++;
+12                currentEnd = farthest;
+13            }
+14        }
+15
+16        return jumps;
+17    }
+18}

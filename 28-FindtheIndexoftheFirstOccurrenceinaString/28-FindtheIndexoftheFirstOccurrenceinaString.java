7ede39fdd2530b1f47a129433ed5f91b4a0c2ead
keerthikroshni <keerthivisu33@gmail.com>
2026-07-21 09:42:01 +0530

    File updated at 7/21/2026, 9:40:25 AM

--- 28-FindtheIndexoftheFirstOccurrenceinaString/28-FindtheIndexoftheFirstOccurrenceinaString.java
+++ 28-FindtheIndexoftheFirstOccurrenceinaString/28-FindtheIndexoftheFirstOccurrenceinaString.java
@@ -1,33 +1,32 @@
-// Last updated: 7/21/2026, 9:39:29 AM
-1class Solution {
-2    List<String> result = new ArrayList<>();
-3
-4    public List<String> letterCombinations(String digits) {
-5        if (digits.length() == 0) {
-6            return result;
-7        }
-8
-9        String[] map = {
-10            "", "", "abc", "def", "ghi", "jkl",
-11            "mno", "pqrs", "tuv", "wxyz"
-12        };
-13
-14        backtrack(digits, 0, new StringBuilder(), map);
-15        return result;
-16    }
+// Last updated: 7/21/2026, 9:40:25 AM
+1/**
+2 * Definition for singly-linked list.
+3 * public class ListNode {
+4 *     int val;
+5 *     ListNode next;
+6 *     ListNode() {}
+7 *     ListNode(int val) { this.val = val; }
+8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+9 * }
+10 */
+11class Solution {
+12    public ListNode swapPairs(ListNode head) {
+13        ListNode dummy = new ListNode(0);
+14        dummy.next = head;
+15
+16        ListNode prev = dummy;
 17
-18    private void backtrack(String digits, int index, StringBuilder current, String[] map) {
-19        if (index == digits.length()) {
-20            result.add(current.toString());
-21            return;
-22        }
-23
-24        String letters = map[digits.charAt(index) - '0'];
+18        while (prev.next != null && prev.next.next != null) {
+19            ListNode first = prev.next;
+20            ListNode second = first.next;
+21
+22            first.next = second.next;
+23            second.next = first;
+24            prev.next = second;
 25
-26        for (char c : letters.toCharArray()) {
-27            current.append(c);
-28            backtrack(digits, index + 1, current, map);
-29            current.deleteCharAt(current.length() - 1);
-30        }
-31    }
-32}
+26            prev = first;
+27        }
+28
+29        return dummy.next;
+30    }
+31}

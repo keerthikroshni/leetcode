869af0625f384075ae358ead85ab79b3e7f7c4a9
keerthikroshni <keerthivisu33@gmail.com>
2026-07-21 09:41:04 +0530

    File updated at 7/21/2026, 9:39:29 AM

--- 28-FindtheIndexoftheFirstOccurrenceinaString/28-FindtheIndexoftheFirstOccurrenceinaString.java
+++ 28-FindtheIndexoftheFirstOccurrenceinaString/28-FindtheIndexoftheFirstOccurrenceinaString.java
@@ -1,21 +1,33 @@
-// Last updated: 7/21/2026, 9:38:14 AM
+// Last updated: 7/21/2026, 9:39:29 AM
 1class Solution {
-2    public int strStr(String haystack, String needle) {
-3        int n = haystack.length();
-4        int m = needle.length();
-5
-6        for (int i = 0; i <= n - m; i++) {
-7            int j = 0;
+2    List<String> result = new ArrayList<>();
+3
+4    public List<String> letterCombinations(String digits) {
+5        if (digits.length() == 0) {
+6            return result;
+7        }
 8
-9            while (j < m && haystack.charAt(i + j) == needle.charAt(j)) {
-10                j++;
-11            }
-12
-13            if (j == m) {
-14                return i;
-15            }
-16        }
+9        String[] map = {
+10            "", "", "abc", "def", "ghi", "jkl",
+11            "mno", "pqrs", "tuv", "wxyz"
+12        };
+13
+14        backtrack(digits, 0, new StringBuilder(), map);
+15        return result;
+16    }
 17
-18        return -1;
-19    }
-20}
+18    private void backtrack(String digits, int index, StringBuilder current, String[] map) {
+19        if (index == digits.length()) {
+20            result.add(current.toString());
+21            return;
+22        }
+23
+24        String letters = map[digits.charAt(index) - '0'];
+25
+26        for (char c : letters.toCharArray()) {
+27            current.append(c);
+28            backtrack(digits, index + 1, current, map);
+29            current.deleteCharAt(current.length() - 1);
+30        }
+31    }
+32}

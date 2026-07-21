bd044f2b762b83ad7e559b03d9ca28c4141437fa
keerthikroshni <keerthivisu33@gmail.com>
2026-07-21 10:01:49 +0530

    File updated at 7/21/2026, 10:00:14 AM

--- 28-FindtheIndexoftheFirstOccurrenceinaString/28-FindtheIndexoftheFirstOccurrenceinaString.java
+++ 28-FindtheIndexoftheFirstOccurrenceinaString/28-FindtheIndexoftheFirstOccurrenceinaString.java
@@ -1,28 +1,17 @@
-// Last updated: 7/21/2026, 9:57:41 AM
+// Last updated: 7/21/2026, 10:00:14 AM
 1class Solution {
-2    List<List<Integer>> result = new ArrayList<>();
-3
-4    public List<List<Integer>> permute(int[] nums) {
-5        backtrack(nums, new ArrayList<>(), new boolean[nums.length]);
-6        return result;
-7    }
-8
-9    private void backtrack(int[] nums, List<Integer> current, boolean[] used) {
-10        if (current.size() == nums.length) {
-11            result.add(new ArrayList<>(current));
-12            return;
-13        }
-14
-15        for (int i = 0; i < nums.length; i++) {
-16            if (used[i]) {
-17                continue;
-18            }
-19
-20            used[i] = true;
-21            current.add(nums[i]);
-22            backtrack(nums, current, used);
-23            current.remove(current.size() - 1);
-24            used[i] = false;
-25        }
-26    }
-27}
+2    public List<List<String>> groupAnagrams(String[] strs) {
+3        Map<String, List<String>> map = new HashMap<>();
+4
+5        for (String s : strs) {
+6            char[] arr = s.toCharArray();
+7            Arrays.sort(arr);
+8            String key = new String(arr);
+9
+10            map.putIfAbsent(key, new ArrayList<>());
+11            map.get(key).add(s);
+12        }
+13
+14        return new ArrayList<>(map.values());
+15    }
+16}

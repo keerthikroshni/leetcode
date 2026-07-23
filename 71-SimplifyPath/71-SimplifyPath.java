7a47a4cb0a234804c61a2226ed590de4feede2f2
keerthikroshni <keerthivisu33@gmail.com>
2026-07-23 09:16:02 +0530

    File updated at 7/23/2026, 9:14:25 AM

--- 71-SimplifyPath/71-SimplifyPath.java
+++ 71-SimplifyPath/71-SimplifyPath.java
@@ -1,27 +1,30 @@
-// Last updated: 7/23/2026, 9:13:22 AM
-1import java.util.*;
-2class Solution {
-3    public String simplifyPath(String path) {
-4        Stack<String> stack = new Stack<>();
-5        String[] parts = path.split("/");
-6        for (String part : parts) {
-7            if (part.equals("") || part.equals(".")) {
-8                continue;
-9            } else if (part.equals("..")) {
-10                if (!stack.isEmpty()) {
-11                    stack.pop();
-12                }
-13            } else {
-14                stack.push(part);
-15            }
-16        }
-17        if (stack.isEmpty()) {
-18            return "/";
-19        }
-20        StringBuilder result = new StringBuilder();
-21        for (String dir : stack) {
-22            result.append("/").append(dir);
-23        }
-24        return result.toString();
-25    }
-26}
+// Last updated: 7/23/2026, 9:14:25 AM
+1class Solution {
+2    public int minDistance(String word1, String word2) {
+3        int m = word1.length();
+4        int n = word2.length();
+5
+6        int[][] dp = new int[m + 1][n + 1];
+7
+8        for (int i = 0; i <= m; i++) {
+9            dp[i][0] = i;
+10        }
+11
+12        for (int j = 0; j <= n; j++) {
+13            dp[0][j] = j;
+14        }
+15
+16        for (int i = 1; i <= m; i++) {
+17            for (int j = 1; j <= n; j++) {
+18                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+19                    dp[i][j] = dp[i - 1][j - 1];
+20                } else {
+21                    dp[i][j] = 1 + Math.min(dp[i - 1][j - 1],
+22                                   Math.min(dp[i - 1][j], dp[i][j - 1]));
+23                }
+24            }
+25        }
+26
+27        return dp[m][n];
+28    }
+29}

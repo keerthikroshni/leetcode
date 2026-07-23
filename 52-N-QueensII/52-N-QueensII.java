98fc96ac88b822f8cf75e43d6987673a4eb99bdf
keerthikroshni <keerthivisu33@gmail.com>
2026-07-23 09:09:01 +0530

    File updated at 7/23/2026, 9:07:24 AM

--- 52-N-QueensII/52-N-QueensII.java
+++ 52-N-QueensII/52-N-QueensII.java
@@ -1,36 +1,20 @@
-// Last updated: 7/23/2026, 9:06:41 AM
+// Last updated: 7/23/2026, 9:07:24 AM
 1class Solution {
-2    public List<Integer> spiralOrder(int[][] matrix) {
-3        List<Integer> result = new ArrayList<>();
+2    public boolean canJump(int[] nums) {
+3        int farthest = 0;
 4
-5        int top = 0;
-6        int bottom = matrix.length - 1;
-7        int left = 0;
-8        int right = matrix[0].length - 1;
+5        for (int i = 0; i < nums.length; i++) {
+6            if (i > farthest) {
+7                return false;
+8            }
 9
-10        while (top <= bottom && left <= right) {
-11            for (int i = left; i <= right; i++) {
-12                result.add(matrix[top][i]);
-13            }
-14            top++;
-15            for (int i = top; i <= bottom; i++) {
-16                result.add(matrix[i][right]);
-17            }
-18            right--;
-19            if (top <= bottom) {
-20                for (int i = right; i >= left; i--) {
-21                    result.add(matrix[bottom][i]);
-22                }
-23                bottom--;
-24            }
-25            if (left <= right) {
-26                for (int i = bottom; i >= top; i--) {
-27                    result.add(matrix[i][left]);
-28                }
-29                left++;
-30            }
-31        }
-32
-33        return result;
-34    }
-35}
+10            farthest = Math.max(farthest, i + nums[i]);
+11
+12            if (farthest >= nums.length - 1) {
+13                return true;
+14            }
+15        }
+16
+17        return true;
+18    }
+19}

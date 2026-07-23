107cf52e7a4b95b05aa90d57a0bc2553152876fc
keerthikroshni <keerthivisu33@gmail.com>
2026-07-23 09:12:30 +0530

    File updated at 7/23/2026, 9:10:53 AM

--- 52-N-QueensII/52-N-QueensII.java
+++ 52-N-QueensII/52-N-QueensII.java
@@ -1,25 +1,36 @@
-// Last updated: 7/23/2026, 9:09:18 AM
-1import java.util.*;
-2class Solution {
-3    public int[][] insert(int[][] intervals, int[] newInterval) {
-4        List<int[]> result = new ArrayList<>();
-5        int i = 0;
-6        int n = intervals.length;
-7        while (i < n && intervals[i][1] < newInterval[0]) {
-8            result.add(intervals[i]);
-9            i++;
-10        }
-11        while (i < n && intervals[i][0] <= newInterval[1]) {
-12            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
-13            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
-14            i++;
-15        }
-16        result.add(newInterval);
-17        while (i < n) {
-18            result.add(intervals[i]);
-19            i++;
-20        }
-21
-22        return result.toArray(new int[result.size()][]);
-23    }
-24}
+// Last updated: 7/23/2026, 9:10:53 AM
+1class Solution {
+2    public int[][] generateMatrix(int n) {
+3        int[][] matrix = new int[n][n];
+4
+5        int top = 0, bottom = n - 1;
+6        int left = 0, right = n - 1;
+7        int num = 1;
+8
+9        while (top <= bottom && left <= right) {
+10
+11            for (int i = left; i <= right; i++) {
+12                matrix[top][i] = num++;
+13            }
+14            top++;
+15            for (int i = top; i <= bottom; i++) {
+16                matrix[i][right] = num++;
+17            }
+18            right--;
+19            if (top <= bottom) {
+20                for (int i = right; i >= left; i--) {
+21                    matrix[bottom][i] = num++;
+22                }
+23                bottom--;
+24            }
+25            if (left <= right) {
+26                for (int i = bottom; i >= top; i--) {
+27                    matrix[i][left] = num++;
+28                }
+29                left++;
+30            }
+31        }
+32
+33        return matrix;
+34    }
+35}

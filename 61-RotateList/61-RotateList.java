869f68b7dc1d8286871db586bbcc72b7da0760c2
keerthikroshni <keerthivisu33@gmail.com>
2026-07-31 08:59:59 +0530

    File updated at 7/31/2026, 8:58:17 AM

--- 61-RotateList/61-RotateList.java
+++ 61-RotateList/61-RotateList.java
@@ -1,16 +1,15 @@
-// Last updated: 7/31/2026, 8:57:20 AM
+// Last updated: 7/31/2026, 8:58:17 AM
 1class Solution {
-2    public List<List<Integer>> subsets(int[] nums) {
-3        List<List<Integer>> ans = new ArrayList<>();
-4        backtrack(0, nums, new ArrayList<>(), ans);
-5        return ans;
-6    }
-7    private void backtrack(int index, int[] nums, List<Integer> temp, List<List<Integer>> ans) {
-8        ans.add(new ArrayList<>(temp));
-9        for (int i = index; i < nums.length; i++) {
-10            temp.add(nums[i]);
-11            backtrack(i + 1, nums, temp, ans);
-12            temp.remove(temp.size() - 1);
-13        }
-14    }
-15}
+2    public int removeDuplicates(int[] nums) {
+3        if (nums.length <= 2)
+4            return nums.length;
+5        int index = 2;
+6        for (int i = 2; i < nums.length; i++) {
+7            if (nums[i] != nums[index - 2]) {
+8                nums[index] = nums[i];
+9                index++;
+10            }
+11        }
+12        return index;
+13    }
+14}

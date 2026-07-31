bbd4dcbf0a471440a11f33b12ea453ef608d6999
keerthikroshni <keerthivisu33@gmail.com>
2026-07-31 09:03:36 +0530

    File updated at 7/31/2026, 9:01:53 AM

--- 61-RotateList/61-RotateList.java
+++ 61-RotateList/61-RotateList.java
@@ -1,22 +1,26 @@
-// Last updated: 7/31/2026, 9:01:10 AM
+// Last updated: 7/31/2026, 9:01:53 AM
 1class Solution {
-2    public List<List<Integer>> subsetsWithDup(int[] nums) {
-3        List<List<Integer>> ans = new ArrayList<>();
-4        Arrays.sort(nums);
-5        backtrack(0, nums, new ArrayList<>(), ans);
-6        return ans;
-7    }
-8
-9    private void backtrack(int index, int[] nums, List<Integer> temp, List<List<Integer>> ans) {
-10        ans.add(new ArrayList<>(temp));
+2    public int numDecodings(String s) {
+3        int n = s.length();
+4
+5        if (n == 0 || s.charAt(0) == '0')
+6            return 0;
+7
+8        int[] dp = new int[n + 1];
+9        dp[0] = 1;
+10        dp[1] = 1;
 11
-12        for (int i = index; i < nums.length; i++) {
-13            if (i > index && nums[i] == nums[i - 1])
-14                continue;
+12        for (int i = 2; i <= n; i++) {
+13            int oneDigit = s.charAt(i - 1) - '0';
+14            int twoDigits = Integer.parseInt(s.substring(i - 2, i));
 15
-16            temp.add(nums[i]);
-17            backtrack(i + 1, nums, temp, ans);
-18            temp.remove(temp.size() - 1);
-19        }
-20    }
-21}
+16            if (oneDigit >= 1)
+17                dp[i] += dp[i - 1];
+18
+19            if (twoDigits >= 10 && twoDigits <= 26)
+20                dp[i] += dp[i - 2];
+21        }
+22
+23        return dp[n];
+24    }
+25}

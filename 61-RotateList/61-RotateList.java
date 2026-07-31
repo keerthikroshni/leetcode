ed872983ce1ca9fda22deabefcdf6e6c3660fa30
keerthikroshni <keerthivisu33@gmail.com>
2026-07-31 08:59:03 +0530

    File updated at 7/31/2026, 8:57:20 AM

--- 61-RotateList/61-RotateList.java
+++ 61-RotateList/61-RotateList.java
@@ -1,19 +1,16 @@
-// Last updated: 7/31/2026, 8:56:35 AM
+// Last updated: 7/31/2026, 8:57:20 AM
 1class Solution {
-2    public List<List<Integer>> combine(int n, int k) {
+2    public List<List<Integer>> subsets(int[] nums) {
 3        List<List<Integer>> ans = new ArrayList<>();
-4        backtrack(1, n, k, new ArrayList<>(), ans);
+4        backtrack(0, nums, new ArrayList<>(), ans);
 5        return ans;
 6    }
-7    private void backtrack(int start, int n, int k, List<Integer> temp, List<List<Integer>> ans) {
-8        if (temp.size() == k) {
-9            ans.add(new ArrayList<>(temp));
-10            return;
-11        }
-12        for (int i = start; i <= n; i++) {
-13            temp.add(i);
-14            backtrack(i + 1, n, k, temp, ans);
-15            temp.remove(temp.size() - 1);
-16        }
-17    }
-18}
+7    private void backtrack(int index, int[] nums, List<Integer> temp, List<List<Integer>> ans) {
+8        ans.add(new ArrayList<>(temp));
+9        for (int i = index; i < nums.length; i++) {
+10            temp.add(nums[i]);
+11            backtrack(i + 1, nums, temp, ans);
+12            temp.remove(temp.size() - 1);
+13        }
+14    }
+15}

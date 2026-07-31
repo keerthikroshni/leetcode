c1857455ec8b5926bdb1e7a0afff813cd98c6383
keerthikroshni <keerthivisu33@gmail.com>
2026-07-31 09:02:53 +0530

    File updated at 7/31/2026, 9:01:10 AM

--- 61-RotateList/61-RotateList.java
+++ 61-RotateList/61-RotateList.java
@@ -1,14 +1,22 @@
-// Last updated: 7/31/2026, 9:00:39 AM
+// Last updated: 7/31/2026, 9:01:10 AM
 1class Solution {
-2    public List<Integer> grayCode(int n) {
-3        List<Integer> ans = new ArrayList<>();
-4
-5        int size = 1 << n;
-6
-7        for (int i = 0; i < size; i++) {
-8            ans.add(i ^ (i >> 1));
-9        }
-10
-11        return ans;
-12    }
-13}
+2    public List<List<Integer>> subsetsWithDup(int[] nums) {
+3        List<List<Integer>> ans = new ArrayList<>();
+4        Arrays.sort(nums);
+5        backtrack(0, nums, new ArrayList<>(), ans);
+6        return ans;
+7    }
+8
+9    private void backtrack(int index, int[] nums, List<Integer> temp, List<List<Integer>> ans) {
+10        ans.add(new ArrayList<>(temp));
+11
+12        for (int i = index; i < nums.length; i++) {
+13            if (i > index && nums[i] == nums[i - 1])
+14                continue;
+15
+16            temp.add(nums[i]);
+17            backtrack(i + 1, nums, temp, ans);
+18            temp.remove(temp.size() - 1);
+19        }
+20    }
+21}

85a32e210ee67512338b73476e8b367bab320773
keerthikroshni <keerthivisu33@gmail.com>
2026-07-31 09:02:22 +0530

    File updated at 7/31/2026, 9:00:39 AM

--- 61-RotateList/61-RotateList.java
+++ 61-RotateList/61-RotateList.java
@@ -1,45 +1,14 @@
-// Last updated: 7/31/2026, 9:00:01 AM
+// Last updated: 7/31/2026, 9:00:39 AM
 1class Solution {
-2    private Map<String, Boolean> memo = new HashMap<>();
-3
-4    public boolean isScramble(String s1, String s2) {
-5        if (s1.equals(s2))
-6            return true;
-7
-8        String key = s1 + "#" + s2;
-9        if (memo.containsKey(key))
-10            return memo.get(key);
-11
-12        int[] count = new int[26];
-13        for (int i = 0; i < s1.length(); i++) {
-14            count[s1.charAt(i) - 'a']++;
-15            count[s2.charAt(i) - 'a']--;
-16        }
-17
-18        for (int c : count) {
-19            if (c != 0) {
-20                memo.put(key, false);
-21                return false;
-22            }
-23        }
-24
-25        int n = s1.length();
-26
-27        for (int i = 1; i < n; i++) {
-28            if (isScramble(s1.substring(0, i), s2.substring(0, i)) &&
-29                isScramble(s1.substring(i), s2.substring(i))) {
-30                memo.put(key, true);
-31                return true;
-32            }
-33
-34            if (isScramble(s1.substring(0, i), s2.substring(n - i)) &&
-35                isScramble(s1.substring(i), s2.substring(0, n - i))) {
-36                memo.put(key, true);
-37                return true;
-38            }
-39        }
-40
-41        memo.put(key, false);
-42        return false;
-43    }
-44}
+2    public List<Integer> grayCode(int n) {
+3        List<Integer> ans = new ArrayList<>();
+4
+5        int size = 1 << n;
+6
+7        for (int i = 0; i < size; i++) {
+8            ans.add(i ^ (i >> 1));
+9        }
+10
+11        return ans;
+12    }
+13}

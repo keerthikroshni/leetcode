2304bd1e50b2017dec53d79fe2d105a8dd3bf442
keerthikroshni <keerthivisu33@gmail.com>
2026-07-31 09:04:15 +0530

    File updated at 7/31/2026, 9:02:33 AM

--- 61-RotateList/61-RotateList.java
+++ 61-RotateList/61-RotateList.java
@@ -1,26 +1,37 @@
-// Last updated: 7/31/2026, 9:01:53 AM
-1class Solution {
-2    public int numDecodings(String s) {
-3        int n = s.length();
-4
-5        if (n == 0 || s.charAt(0) == '0')
-6            return 0;
-7
-8        int[] dp = new int[n + 1];
-9        dp[0] = 1;
-10        dp[1] = 1;
-11
-12        for (int i = 2; i <= n; i++) {
-13            int oneDigit = s.charAt(i - 1) - '0';
-14            int twoDigits = Integer.parseInt(s.substring(i - 2, i));
+// Last updated: 7/31/2026, 9:02:33 AM
+1/**
+2 * Definition for singly-linked list.
+3 * public class ListNode {
+4 *     int val;
+5 *     ListNode next;
+6 *     ListNode() {}
+7 *     ListNode(int val) { this.val = val; }
+8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+9 * }
+10 */
+11class Solution {
+12    public ListNode reverseBetween(ListNode head, int left, int right) {
+13        if (head == null || left == right)
+14            return head;
 15
-16            if (oneDigit >= 1)
-17                dp[i] += dp[i - 1];
+16        ListNode dummy = new ListNode(0);
+17        dummy.next = head;
 18
-19            if (twoDigits >= 10 && twoDigits <= 26)
-20                dp[i] += dp[i - 2];
-21        }
-22
-23        return dp[n];
-24    }
-25}
+19        ListNode prev = dummy;
+20
+21        for (int i = 1; i < left; i++) {
+22            prev = prev.next;
+23        }
+24
+25        ListNode curr = prev.next;
+26
+27        for (int i = 0; i < right - left; i++) {
+28            ListNode next = curr.next;
+29            curr.next = next.next;
+30            next.next = prev.next;
+31            prev.next = next;
+32        }
+33
+34        return dummy.next;
+35    }
+36}

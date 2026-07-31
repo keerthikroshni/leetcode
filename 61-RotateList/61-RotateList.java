fab8cb1b504396b2d2952687c70512e7177683b9
keerthikroshni <keerthivisu33@gmail.com>
2026-07-31 08:56:49 +0530

    File updated at 7/31/2026, 8:55:06 AM

--- 61-RotateList/61-RotateList.java
+++ 61-RotateList/61-RotateList.java
@@ -1,25 +1,25 @@
-// Last updated: 7/31/2026, 8:53:52 AM
+// Last updated: 7/31/2026, 8:55:06 AM
 1class Solution {
-2    public boolean searchMatrix(int[][] matrix, int target) {
-3        int m = matrix.length;
-4        int n = matrix[0].length;
-5
-6        int left = 0;
-7        int right = m * n - 1;
-8
-9        while (left <= right) {
-10            int mid = left + (right - left) / 2;
-11
-12            int value = matrix[mid / n][mid % n];
-13
-14            if (value == target)
-15                return true;
-16            else if (value < target)
-17                left = mid + 1;
-18            else
-19                right = mid - 1;
-20        }
-21
-22        return false;
+2    public void sortColors(int[] nums) {
+3        int low = 0;
+4        int mid = 0;
+5        int high = nums.length - 1;
+6
+7        while (mid <= high) {
+8            if (nums[mid] == 0) {
+9                int temp = nums[low];
+10                nums[low] = nums[mid];
+11                nums[mid] = temp;
+12                low++;
+13                mid++;
+14            } else if (nums[mid] == 1) {
+15                mid++;
+16            } else {
+17                int temp = nums[mid];
+18                nums[mid] = nums[high];
+19                nums[high] = temp;
+20                high--;
+21            }
+22        }
 23    }
 24}

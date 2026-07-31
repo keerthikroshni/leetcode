0f8185162693449416013996be05872e9caadc5d
keerthikroshni <keerthivisu33@gmail.com>
2026-07-31 09:10:24 +0530

    File updated at 7/31/2026, 9:08:42 AM

--- 115-DistinctSubsequences/115-DistinctSubsequences.java
+++ 115-DistinctSubsequences/115-DistinctSubsequences.java
@@ -1,21 +1,50 @@
-// Last updated: 7/31/2026, 9:05:08 AM
+// Last updated: 7/31/2026, 9:08:42 AM
 1class Solution {
-2    public int numDistinct(String s, String t) {
-3        int m = s.length();
-4        int n = t.length();
-5        int[][] dp = new int[m + 1][n + 1];
-6        for (int i = 0; i <= m; i++) {
-7            dp[i][0] = 1;
-8        }
-9        for (int i = 1; i <= m; i++) {
-10            for (int j = 1; j <= n; j++) {
-11                if (s.charAt(i - 1) == t.charAt(j - 1)) {
-12                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
-13                } else {
-14                    dp[i][j] = dp[i - 1][j];
-15                }
-16            }
-17        }
-18        return dp[m][n];
-19    }
-20}
+2    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+3        Set<String> dict = new HashSet<>(wordList);
+4
+5        if (!dict.contains(endWord))
+6            return 0;
+7
+8        Queue<String> queue = new LinkedList<>();
+9        queue.offer(beginWord);
+10
+11        Set<String> visited = new HashSet<>();
+12        visited.add(beginWord);
+13
+14        int level = 1;
+15
+16        while (!queue.isEmpty()) {
+17            int size = queue.size();
+18
+19            for (int i = 0; i < size; i++) {
+20                String word = queue.poll();
+21
+22                if (word.equals(endWord))
+23                    return level;
+24
+25                char[] arr = word.toCharArray();
+26
+27                for (int j = 0; j < arr.length; j++) {
+28                    char old = arr[j];
+29
+30                    for (char c = 'a'; c <= 'z'; c++) {
+31                        arr[j] = c;
+32                        String next = new String(arr);
+33
+34                        if (dict.contains(next) && !visited.contains(next)) {
+35                            visited.add(next);
+36                            queue.offer(next);
+37                        }
+38                    }
+39
+40                    arr[j] = old;
+41                }
+42            }
+43
+44            level++;
+45        }
+46
+47        return 0;
+48    }
+49}

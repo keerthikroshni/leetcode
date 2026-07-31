7ed6c2beb4221b468b93c2d9583fc22ef135283e
keerthikroshni <keerthivisu33@gmail.com>
2026-07-31 09:00:50 +0530

    File updated at 7/31/2026, 8:59:08 AM

--- 61-RotateList/61-RotateList.java
+++ 61-RotateList/61-RotateList.java
@@ -1,15 +1,33 @@
-// Last updated: 7/31/2026, 8:58:17 AM
-1class Solution {
-2    public int removeDuplicates(int[] nums) {
-3        if (nums.length <= 2)
-4            return nums.length;
-5        int index = 2;
-6        for (int i = 2; i < nums.length; i++) {
-7            if (nums[i] != nums[index - 2]) {
-8                nums[index] = nums[i];
-9                index++;
-10            }
-11        }
-12        return index;
-13    }
-14}
+// Last updated: 7/31/2026, 8:59:08 AM
+1/**
+2 * Definition for singly-linked list.
+3 * public class ListNode {
+4 *     int val;
+5 *     ListNode next;
+6 *     ListNode() {}
+7 *     ListNode(int val) { this.val = val; }
+8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+9 * }
+10 */
+11class Solution {
+12    public ListNode deleteDuplicates(ListNode head) {
+13        ListNode dummy = new ListNode(0);
+14        dummy.next = head;
+15
+16        ListNode prev = dummy;
+17
+18        while (head != null) {
+19            if (head.next != null && head.val == head.next.val) {
+20                while (head.next != null && head.val == head.next.val) {
+21                    head = head.next;
+22                }
+23                prev.next = head.next;
+24            } else {
+25                prev = prev.next;
+26            }
+27            head = head.next;
+28        }
+29
+30        return dummy.next;
+31    }
+32}

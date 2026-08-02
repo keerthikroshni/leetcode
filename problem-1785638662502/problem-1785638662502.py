45044a75a11d197042b1b419b98cdb240998d020
keerthikroshni <keerthivisu33@gmail.com>
2026-08-02 08:16:08 +0530

    Create

---
+++ problem-1785638662502/problem-1785638662502.py
@@ -0,0 +1,17 @@
+# Last updated: 8/2/2026, 8:14:22 AM
+1
+2class Solution(object):
+3    def gcd(self,a,b):
+4        while b:
+5            a,b=b,a%b
+6        return a
+7    def maxPairStrength(self, nums):
+8        ans=0
+9        n=len(nums)
+10        for i in range(n):
+11            for j in range(i+1,n):
+12                g=self.gcd(nums[i],nums[j])
+13                strength=(nums[i]*nums[j])//(g*g)
+14                ans=max(ans,strength)
+15        return ans
+16        

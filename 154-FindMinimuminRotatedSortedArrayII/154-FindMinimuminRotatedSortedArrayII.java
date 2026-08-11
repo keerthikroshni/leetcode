d25792182f1f21be882a5eedc085e16a0e1e0d9e
keerthikroshni <keerthivisu33@gmail.com>
2026-08-11 09:26:05 +0530

    File updated at 8/11/2026, 9:24:14 AM

--- 154-FindMinimuminRotatedSortedArrayII/154-FindMinimuminRotatedSortedArrayII.java
+++ 154-FindMinimuminRotatedSortedArrayII/154-FindMinimuminRotatedSortedArrayII.java
@@ -1,22 +1,19 @@
-// Last updated: 8/11/2026, 9:23:30 AM
+// Last updated: 8/11/2026, 9:24:14 AM
 1class Solution {
-2    public int findMin(int[] nums) {
+2    public int findPeakElement(int[] nums) {
 3        int left = 0;
 4        int right = nums.length - 1;
 5
 6        while (left < right) {
 7            int mid = left + (right - left) / 2;
 8
-9            if (nums[mid] > nums[right]) {
-10                left = mid + 1;
-11            } else if (nums[mid] < nums[right]) {
-12                right = mid;
-13            } else {
-14              
-15                right--;
-16            }
-17        }
-18
-19        return nums[left];
-20    }
-21}
+9            if (nums[mid] > nums[mid + 1]) {
+10                right = mid;
+11            } else {
+12                left = mid + 1;
+13            }
+14        }
+15
+16        return left;
+17    }
+18}

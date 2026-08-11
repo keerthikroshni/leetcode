e42dcfbc2065547bd205b4a63514e3d24a7675e6
keerthikroshni <keerthivisu33@gmail.com>
2026-08-11 09:26:43 +0530

    File updated at 8/11/2026, 9:24:53 AM

--- 154-FindMinimuminRotatedSortedArrayII/154-FindMinimuminRotatedSortedArrayII.java
+++ 154-FindMinimuminRotatedSortedArrayII/154-FindMinimuminRotatedSortedArrayII.java
@@ -1,19 +1,21 @@
-// Last updated: 8/11/2026, 9:24:14 AM
+// Last updated: 8/11/2026, 9:24:53 AM
 1class Solution {
-2    public int findPeakElement(int[] nums) {
+2    public int[] twoSum(int[] numbers, int target) {
 3        int left = 0;
-4        int right = nums.length - 1;
+4        int right = numbers.length - 1;
 5
 6        while (left < right) {
-7            int mid = left + (right - left) / 2;
+7            int sum = numbers[left] + numbers[right];
 8
-9            if (nums[mid] > nums[mid + 1]) {
-10                right = mid;
-11            } else {
-12                left = mid + 1;
-13            }
-14        }
-15
-16        return left;
-17    }
-18}
+9            if (sum == target) {
+10                return new int[]{left + 1, right + 1};
+11            } else if (sum < target) {
+12                left++;
+13            } else {
+14                right--;
+15            }
+16        }
+17
+18        return new int[]{-1, -1};
+19    }
+20}

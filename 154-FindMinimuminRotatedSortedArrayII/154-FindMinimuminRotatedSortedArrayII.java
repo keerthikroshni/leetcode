2f6cebe185df79a4b7abe13a284b687e61b59026
keerthikroshni <keerthivisu33@gmail.com>
2026-08-11 09:28:17 +0530

    File updated at 8/11/2026, 9:26:27 AM

--- 154-FindMinimuminRotatedSortedArrayII/154-FindMinimuminRotatedSortedArrayII.java
+++ 154-FindMinimuminRotatedSortedArrayII/154-FindMinimuminRotatedSortedArrayII.java
@@ -1,19 +1,41 @@
-// Last updated: 8/11/2026, 9:25:35 AM
-1class Solution {
-2    public int minSubArrayLen(int target, int[] nums) {
-3        int left = 0;
-4        int sum = 0;
-5        int minLen = Integer.MAX_VALUE;
-6
-7        for (int right = 0; right < nums.length; right++) {
-8            sum += nums[right];
-9
-10            while (sum >= target) {
-11                minLen = Math.min(minLen, right - left + 1);
-12                sum -= nums[left++];
-13            }
-14        }
-15
-16        return minLen == Integer.MAX_VALUE ? 0 : minLen;
-17    }
-18}
+// Last updated: 8/11/2026, 9:26:27 AM
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    public int countNodes(TreeNode root) {
+18        if (root == null) {
+19            return 0;
+20        }
+21
+22        int leftHeight = getLeftHeight(root.left);
+23        int rightHeight = getLeftHeight(root.right);
+24
+25        if (leftHeight == rightHeight) {
+26            return (1 << leftHeight) + countNodes(root.right);
+27        } else {
+28            return (1 << rightHeight) + countNodes(root.left);
+29        }
+30    }
+31
+32    private int getLeftHeight(TreeNode node) {
+33        int height = 0;
+34        while (node != null) {
+35            height++;
+36            node = node.left;
+37        }
+38        return height;
+39    }
+40}
